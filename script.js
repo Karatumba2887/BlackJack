2718b9dfad3962494a023b923ee4a7285210c285
Maria <mashachan55@gmail.com>
2023-05-02 18:26:02 +0200

    project

---
+++ script.js
@@ -0,0 +1,125 @@
+//let cards = [2,3,4,5,6,7,8,9,10,V,D,K,A]
+
+let images = document.createElement("img");
+images.src = "./pngwing.com.png";
+images.width = "1000";
+images.height = "550";
+let div1 = document.createElement("div");
+div1.append(images);
+document.body.append(div1);
+
+
+
+  //let twoS = new Object();
+
+ // let twoS = {
+	//neme: Spades
+	//cost: 2;
+ // cart: images;
+ // }
+//console.log(twoS);
+
+
+// let threeS = new Object();
+  
+//   let threeS = {
+// 	neme: Spades;
+// 	cost: 3;
+//   }
+
+
+//   let fourS = new Object();
+  
+//   let fourS = {
+// 	neme: Spades;
+// 	cost: 4;
+//   }
+
+
+//  let fiveS = new Object();
+  
+//   let fiveS = {
+// 	neme: Spades;
+// 	cost: 5;
+//   }
+  
+
+//   let sixS = new Object();
+  
+//   let sixS = {
+// 	neme: Spades;
+// 	cost: 6;
+//   }
+
+
+
+//   let sevenS = new Object();
+  
+//   let sevenS = {
+// 	neme: Spades;
+// 	cost: 7;
+//   }
+
+
+
+//   let eightS = new Object();
+  
+//   let eightS = {
+// 	neme: Spades;
+// 	cost: 8;
+//   }
+
+
+
+//   let nineS = new Object();
+  
+//   let nineS = {
+// 	neme: Spades;
+// 	cost: 9;
+//   }
+
+
+
+//   let teS = new Object();
+  
+//   let tenS = {
+// 	neme: Spades;
+// 	cost: 10;
+//   }
+
+
+
+//   let jackS = new Object();
+  
+//   let jackS = {
+// 	neme: Spades;
+// 	cost: 11;
+//   }
+
+
+
+//   let queenS = new Object();
+  
+//   let qeenS = {
+// 	neme: Spades;
+// 	cost: 12;
+//   }
+
+
+
+//   let kingS = new Object();
+  
+//   let kingS = {
+// 	neme: Spades;
+// 	cost: 13;
+//   }
+
+
+
+//   let aceS = new Object();
+  
+//   let ace1S = {
+// 	neme: Spades;
+// 	cost: 1;
+//   } 
+
