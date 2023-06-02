3d1f6179027a7769c63da4d50a0fe90b282c92c8
Maria <mashachan55@gmail.com>
2023-06-02 16:49:56 +0200

    I added functions for cards and changed HTML code.

--- JS/script.js
+++ JS/script.js
@@ -1,13 +1,50 @@
 
-function addRandomCards() {
-    // Создаем массив всех доступных мастей и значений
-    const suits = ['clubs', 'diamonds', 'hearts', 'spades'];
-    const values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'];
+  const suits = ['♣️', '♦️', '♥️', '♠️'];
+  const values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']; //arrays
 
-    const cardsContainer = document.querySelector('.cards-container');
-    const cards = Array.from(cardsContainer.getElementsByClassName('card'));
+
+   const deck = [];    //array for the deck
+
+    for (let suit of suits) {           //cycle that creating objects for cards
+        for (let value of values) {
+         const card = { suit, value };
+         deck.push(card);
+        }
+     }
+
+function getRandomObject(cards) {       //function for mixing cards
+     const randomObject = Math.floor(Math.random() * cards.length);
+     return cards[randomObject];
+     }
+
+     const randomCart = getRandomObject(deck)
+
+function newCard(){
+    const elementCard = document.createElement("div");
+    elementCard.className = "card";
+
+    const elementTopLeft = document.createElement("div");
+    elementTopLeft.className = "top-left";
+    elementTopLeft.innerText = randomCart.value;
+
+    const elementBottomRight = document.createElement("div");
+    elementBottomRight.className = "bottom-right";
+    elementBottomRight.innerText = randomCart.value;
+
+    const elementSuit = document.createElement("div");
+    elementSuit.className = "suit";
+    elementSuit.innerText = randomCart.suit;
+
+ elementCard.appendChild(elementTopLeft);
+ elementCard.appendChild(elementBottomRight);
+ elementCard.appendChild(elementSuit);
+
+ return elementCard;
 
 }
+ const container = document.getElementById('cards-container');
+ const cardElement = newCard();
+ container.appendChild(cardElement);
 
 
 
@@ -30,126 +67,33 @@ function addRandomCards() {
 
 
 
-// let images = document.createElement("img");
-// images.src = "./pngwing.com.png";
-// images.width = "1000";
-// images.height = "550";
-// let div1 = document.createElement("div");
-// div1.append(images);
-// document.body.append(div1);
 
 
 
-  //let twoS = new Object();
-
- // let twoS = {
-	//neme: Spades
-	//cost: 2;
- // cart: images;
- // }
-//console.log(twoS);
-
-
-// let threeS = new Object();
-  
-//   let threeS = {
-// 	neme: Spades;
-// 	cost: 3;
-//   }
-
-
-//   let fourS = new Object();
-  
-//   let fourS = {
-// 	neme: Spades;
-// 	cost: 4;
-//   }
-
-
-//  let fiveS = new Object();
-  
-//   let fiveS = {
-// 	neme: Spades;
-// 	cost: 5;
-//   }
-  
-
-//   let sixS = new Object();
-  
-//   let sixS = {
-// 	neme: Spades;
-// 	cost: 6;
-//   }
 
 
 
-//   let sevenS = new Object();
-  
-//   let sevenS = {
-// 	neme: Spades;
-// 	cost: 7;
-//   }
 
 
 
-//   let eightS = new Object();
-  
-//   let eightS = {
-// 	neme: Spades;
-// 	cost: 8;
-//   }
 
 
 
-//   let nineS = new Object();
-  
-//   let nineS = {
-// 	neme: Spades;
-// 	cost: 9;
-//   }
 
 
 
-//   let teS = new Object();
-  
-//   let tenS = {
-// 	neme: Spades;
-// 	cost: 10;
-//   }
 
 
 
-//   let jackS = new Object();
-  
-//   let jackS = {
-// 	neme: Spades;
-// 	cost: 11;
-//   }
 
 
 
-//   let queenS = new Object();
-  
-//   let qeenS = {
-// 	neme: Spades;
-// 	cost: 12;
-//   }
 
 
 
-//   let kingS = new Object();
-  
-//   let kingS = {
-// 	neme: Spades;
-// 	cost: 13;
-//   }
 
 
 
-//   let aceS = new Object();
-  
-//   let ace1S = {
-// 	neme: Spades;
-// 	cost: 1;
-//   } 
+
+
 
